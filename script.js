77079ce1e87ca6d5d421ef3baa7a23f216a53722
sonthb3992 <71615947+sonthb3992@users.noreply.github.com>
2023-05-11 02:34:34 +0000

    Create the bartender

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-alert("Loaded");
+const addons = [
+    { "name": "Whipped cream", "price": 0.5 },
+    { "name": "Whipped cream", "price": 0.5 },
+];
+
+const bartender = {
+    createDrink: (type) => {
+        function createDrink(type) {
+            const drink = {
+                type: type,
+                price: 2.00,
+                hasLarge: false,
+                addons: [],
+                getDesc: function() {
+                    return `Welcome ${type}, with ${addons}!`;
+                }
+            };
+            if (type === 'cold' || type === 'blended') {
+                drink.hasLarge = true;
+            }
+            return drink;
+        }
+    }
+}
+
+let drink = bartender.createDrink("hot");
+Console.log(drink.getDesc());
